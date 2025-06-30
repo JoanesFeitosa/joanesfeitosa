59ab37088e504567adaba9604658a123fb31e3bf
Joatan Feitosa <joatancarlos@gmail.com>
2025-06-30 13:36:37 -0300

    botao voltar

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const page = pathname.substring(pathname.lastIndexOf("/") + 1);
 
 if (page !== "index.html" && page !== "") {
     const btn = document.getElementById("backToIndex");
-    btn.style.display = "inline-block";
+    btn.style.display = "flex";
     btn.addEventListener("click", () => {
         window.location.href = "../index.html";
     });
